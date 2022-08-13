efe0d93e420881d4f7c51a94f425ad4a05a0e787
Thomas <collinskthomas@gmail.com>
2022-08-13 12:34:42 +0000

    Implent playGame() & determinWinner() functions

--- game.js
+++ game.js
@@ -12,26 +12,51 @@ let getComputerChoice = function () {
 let playRound = function (playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
-   if (playerSelection == 'rock' && computerSelection == 'rock') {
-     return "Draw! Rock can't beat Rock";
-   } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-     return 'You Lose! Paper beats Rock';
-   } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-     return 'You Win! Rock beats Scissors';
-   } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-     return 'You Win! Paper beats Rock';
-   } else if (playerSelection == 'paper' && computerSelection == 'paper') {
-     return "Draw! Paper can't beat Paper";
-   } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-     return 'You Lose! Scissors beats Paper';
-   } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-     return 'You Lose! Rock beats Scissors';
-   } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-     return 'You Win! Scissors beats Paper';
-   } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-     return "Draw! Scissors can't beat Scissors";
-   }
+  if (playerSelection == 'rock' && computerSelection == 'rock') {
+    return { result: 'd', message: "Draw! Rock can't beat Rock" };
+  } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return { result: 'l', message: 'You Lose! Paper beats Rock' };
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return { result: 'w', message: 'You Win! Rock beats Scissors' };
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return { result: 'w', message: 'You Win! Paper beats Rock' };
+  } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+    return { result: 'd', message: "Draw! Paper can't beat Paper" };
+  } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    return { result: 'l', message: 'You Lose! Scissors beats Paper' };
+  } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    return { result: 'l', message: 'You Lose! Rock beats Scissors' };
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    return { result: 'w', message: 'You Win! Scissors beats Paper' };
+  } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+    return { result: 'd', message: "Draw! Scissors can't beat Scissors" };
+  }
 };
 
-let playGame = function () {};
+let determineWinner = function (playerScore) {
+  if (playerScore == 5) {
+    return 'You Won!';
+  } else {
+    return 'You Lost!';
+  }
+};
 
+let playGame = function () {
+  let computerScore = 0;
+  let playerScore = 0;
+
+  while (playerScore < 5 && computerScore < 5) {
+    let gameOutcome = playRound('rock', getComputerChoice());
+    if (gameOutcome.result == 'w') {
+      playerScore++;
+    } else if (gameOutcome.result == 'l') {
+      computerScore++;
+    }
+    console.log(gameOutcome.message);
+  }
+
+  console.log(`${determineWinner(playerScore)}
+You:${playerScore} - Computer:${computerScore}`);
+};
+
+playGame();

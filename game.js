e8ef5517b0bc52b029e355c66fe0e54ec2bf5eca
Thomas <collinskthomas@gmail.com>
2022-08-13 11:30:46 +0000

    Implement the getComputerChoice() function

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
-let getComputerChoice = function (){
+let getComputerChoice = function () {
+  let randomChoice = Math.floor(Math.random() * 3);
+  if (randomChoice == 0) {
+    return 'ROCK';
+  } else if (randomChoice == 1) {
+    return 'PAPER';
+  } else {
+    return 'SCISSORS';
+  }
+};
 
-}
+let playRound = function () {};
 
-let playRound = function (){
-
-}
-
-let playGame = function (){
-
-}
+let playGame = function () {};
